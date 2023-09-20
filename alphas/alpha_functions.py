0b759b0ffd5805dff23aca451c074e37ede1344b
Yash patel <113195427+yyashhp@users.noreply.github.com>
2023-09-19 20:00:56 -0400

    alpha.utils file

--- alphas/alpha_functions.py
+++ alphas/alpha_functions.py
@@ -33,19 +33,4 @@ def alpha_001(data: pd.DataFrame, ticker: str) -> pd.DataFrame:
     raw_signal.loc[drop_signal_indices] = 0
     return raw_signal
 
-def alpha_042(data: pd.DataFrame, ticker: str) -> pd.DataFrame:
-    '''
-    minus(
-    csrank(kentau_10(tsrank_10(mvwap_5()),tsrank_10(obv_5()))),
-    csrank(logret_10())
-
-    Args: pd.DataFrame
-        Data: requires "Adj Close" and "Volume"
-    '''
-
-    x = data['Close'].rolling(window=2).mean().dropna()[6:] > (data['Close'].rolling(window=8).mean().dropna() + data['Close'].rolling(window=8).std().dropna())
-    csrank = data["Volume"] / np.sign(((data['Adj Close'] / data['Adj Close'].shift(1) - 1) * data['Volume']).dropna()).rolling(window=20).sum().dropna()
-    raw_signal = [-1 if val else 1 for val in x][13:] * scipy.stats.rankdata(csrank, method="average", nan_policy="omit")[
-                                                   20:]
-    return raw_signal
 
